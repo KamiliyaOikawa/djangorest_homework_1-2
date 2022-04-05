3bb4e614db21a025f1c6c72eead81a452537f8ac
KamiliyaOikawa <knasyrova622@gmail.com>
2022-04-05 17:28:43 +0600

    3-5 homework

--- afisha/urls.py
+++ afisha/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from movie_app import views
+from users import views as user_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,6 @@ urlpatterns = [
     path('api/v1/reviews/', views.review_list_view),
     path('api/v1/reviews/<int:id>/', views.review_detail_view),
     path('api/v1/movies/reviews/', views.movies_reviews_view),
+    path('api/v1/register/', user_views.registration),
+    path('api/v1/login/', user_views.authorization)
 ]
